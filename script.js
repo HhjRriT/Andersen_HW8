10146352bff30d222805c537e4b495fa133206a7
HhJRriT <artkol0709@gmail.com>
2021-06-05 04:35:42 +0300

    ohhh myyy

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let screen  = (document.body.querySelector(".screen"))
-const btns  = document.body.querySelector(".buttons")
+let screen = (document.body.querySelector(".screen"))
+const btns = document.body.querySelector(".buttons")
 let res = ""
 let value = ""
 let lastValue = ""
 let lastOper = ""
 let operator = ""
+let fin = false
 const maxInput = 10
 
 function updateScreen(num) {
-    if (!num) return  screen.innerText = res
-    if (num.length > maxInput) return  screen.innerText = (+num).toExponential(maxInput - 5)
+    if (num === "error") {
+        screen.innerText = "Error /0"
+        res = ""
+        value = ""
+        operator = ""
+    }
+    if (num.length > maxInput) return screen.innerText = (+num).toExponential(maxInput - 5)
     screen.innerText = num
 }
 
-const allButons = [[7,8,9,"/"],[4,5,6,"-"], [1,2,3,"+"],[".",0,"=", "x"]]
+const allButons = [[7, 8, 9, "/"], [4, 5, 6, "-"], [1, 2, 3, "+"], [".", 0, "=", "x"]]
 
-function renderBtns (arr) {
+function renderBtns(arr) {
     for (row of arr) {
         const line = document.createElement("div")
         line.className = "row"
@@ -30,11 +36,10 @@ function renderBtns (arr) {
 
 btns.addEventListener("click", (event) => {
     if (event.target.tagName === "BUTTON") {
-        if (+event.target.innerText >= 0 && +event.target.innerText <= 9) return  numbers(event.target.innerText)
+        if (+event.target.innerText >= 0 && +event.target.innerText <= 9) return numbers(event.target.innerText)
         if (event.target.innerText === "=") {
             if (!value && !lastValue) return res = ""
             if (!value && lastValue) {
-                console.log("yes")
                 value = lastValue
                 operator = lastOper
                 return operation()
@@ -42,23 +47,30 @@ btns.addEventListener("click", (event) => {
         }
         operation(event.target.innerText)
     }
-
 })
 
 btns.addEventListener("click", (event) => {
-    console.log("result     -",res)
-    console.log("operator   -",operator)
-    console.log("value      -",value)
+    console.log("result     -", res)
+    console.log("operator   -", operator)
+    console.log("value      -", value)
     console.log("lastValue  -", lastValue)
     console.log("lastOper  -", lastOper)
+    console.log(fin)
     console.log("---------------------")
 })
 
 
 function numbers(num) {
+    if (fin) {
+        fin = false
+        res = ""
+        lastValue = ""
+        operator = ""
+        lastOper = ""
+    }
     if (!operator) {
         res.length < maxInput && updateScreen(res = res + num)
-    }else if (value.length < maxInput) updateScreen(value = value +num)
+    } else if (value.length < maxInput) updateScreen(value = value + num)
 }
 
 function operation(oper) {
@@ -76,34 +88,28 @@ function operation(oper) {
     }
     if (operator === "+") {
         updateScreen(res = +res + +value + "")
+        fin = true
         lastOper = operator
     }
     if (operator === "-") {
         updateScreen(res = +res - +value + "")
+        fin = true
         lastOper = operator
-
     }
     if (operator === "x") {
         updateScreen(res = +res * +value + "")
+        fin = true
         lastOper = operator
-         }
-
+    }
     if (operator === "/") {
+        if (value === "0") return updateScreen("error")
         updateScreen(res = +res / +value + "")
+        fin = true
         lastOper = operator
-       }
-
-    if (operator === "=") {
-        operator = ""
-        return updateScreen(operation(operator))
     }
     lastValue = value
     value = ""
 }
 
-function extra () {
-
-}
-
-updateScreen()
+updateScreen("")
 renderBtns(allButons)
